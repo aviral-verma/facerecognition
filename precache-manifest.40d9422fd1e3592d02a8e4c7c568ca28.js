f9534d026d8b00800e190235ecb034ba830ba830
Aviral Verma <aviralverma.verma@gmail.com>
2019-05-26 01:26:49 +0530

    Updates

---
+++ precache-manifest.40d9422fd1e3592d02a8e4c7c568ca28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83147391232c79266f6165c103be341b",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "52f75576a8983a8361d7",
+    "url": "/facerecognition/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "b865a4c5077a13133e7d",
+    "url": "/facerecognition/static/css/main.6aeda91b.chunk.css"
+  },
+  {
+    "revision": "52f75576a8983a8361d7",
+    "url": "/facerecognition/static/js/2.c9086818.chunk.js"
+  },
+  {
+    "revision": "b865a4c5077a13133e7d",
+    "url": "/facerecognition/static/js/main.be69da1f.chunk.js"
+  },
+  {
+    "revision": "9a6a6a0c42958dd8e526",
+    "url": "/facerecognition/static/js/runtime~main.7f9ce1ea.js"
+  }
+]);
